563ce0dda2c2604b4966e311fb6e50edfba59122
Cornelius Motanya <corneliusmot@yahoo.com>
2025-04-23 18:29:32 +0300

    tried to remove errors by adding subset to poppins font in fonts.ts

--- app/data/font.ts
+++ app/data/font.ts
@@ -13,6 +13,7 @@ export const grotesque = Grotesque({
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 export const bigShoulder = BigShoulder({
