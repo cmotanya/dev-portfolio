47020f15fb517888647e307b49bd7d25b82df141
Cornelius Motanya <corneliusmot@yahoo.com>
2025-04-27 12:07:48 +0300

    refactor: Restore and implement Header component with necessary imports and functionality

--- app/data/font.ts
+++ app/data/font.ts
@@ -1,4 +1,8 @@
-import { Bricolage_Grotesque as Grotesque, Poppins } from "next/font/google";
+import {
+  Bricolage_Grotesque as Grotesque,
+  Caveat_Brush as Caveat,
+  Poppins,
+} from "next/font/google";
 
 export const grotesque = Grotesque({
   variable: "--font-geist-sans",
@@ -9,4 +13,10 @@ export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+export const caveat = Caveat({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  weight: "400",
 });
--- app/header/page.tsx
+++ app/header/page.tsx
@@ -1,139 +1,139 @@
-// "use client";
+"use client";
 
-// import React, { useState } from "react";
-// import { nav } from "../data/nav";
-// import Link from "next/link";
-// import Image from "next/image";
-// import { socials } from "../data/socials";
-// import { ChevronRight, Sun } from "lucide-react";
-// import MenuToggleButton from "../../components/menu-toggle-button";
-// import { cn } from "@/lib/utils";
-// import { caveat } from "../data/font";
-// import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { nav } from "../data/nav";
+import Link from "next/link";
+import Image from "next/image";
+import { socials } from "../data/socials";
+import { ChevronRight, Sun } from "lucide-react";
+import MenuToggleButton from "../../components/menu-toggle-button";
+import { cn } from "@/lib/utils";
+import { caveat } from "../data/font";
+import { usePathname, useRouter } from "next/navigation";
 
-// const Header = () => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const router = useRouter();
-//   const pathname = usePathname();
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-//   const handleCloseMenu = () => {
-//     setIsMenuOpen(false);
-//   };
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
 
-//   const handleResumeClick = () => {
-//     setIsMenuOpen(false);
-//     router.push("/resume");
-//   };
+  const handleResumeClick = () => {
+    setIsMenuOpen(false);
+    router.push("/resume");
+  };
 
-//   return (
-//     <>
-//       {/* Background overlay */}
-//       {isMenuOpen && (
-//         <div
-//           onClick={toggleMenu}
-//           aria-hidden="true"
-//           className="fixed inset-0 z-10 h-screen bg-gray-300/50 backdrop-blur-[1.5px]"
-//         />
-//       )}
+  return (
+    <>
+      {/* Background overlay */}
+      {isMenuOpen && (
+        <div
+          onClick={toggleMenu}
+          aria-hidden="true"
+          className="fixed inset-0 z-10 h-screen bg-gray-300/50 backdrop-blur-[1.5px]"
+        />
+      )}
 
-//       <header
-//         className={cn(
-//           "fixed z-[999] flex h-screen w-[60%] flex-col overflow-hidden shadow-md backdrop-blur-md transition-transform duration-300 ease-in-out",
-//           isMenuOpen ? "translate-x-0" : "-translate-x-full",
-//         )}
-//       >
-//         {/* Profile section */}
-//         <div className="mx-3 my-6 flex items-center gap-3 border-b pb-4">
-//           <Image
-//             src="/avatar.jpg"
-//             alt="Cornelius Motanya"
-//             width={64}
-//             height={64}
-//             className="size-16 rounded-full border-2 border-blue-500 object-cover object-center"
-//           />
+      <header
+        className={cn(
+          "fixed z-[999] flex h-screen w-[60%] flex-col overflow-hidden shadow-md backdrop-blur-md transition-transform duration-300 ease-in-out",
+          isMenuOpen ? "translate-x-0" : "-translate-x-full",
+        )}
+      >
+        {/* Profile section */}
+        <div className="mx-3 my-6 flex items-center gap-3 border-b pb-4">
+          <Image
+            src="/avatar.jpg"
+            alt="Cornelius Motanya"
+            width={64}
+            height={64}
+            className="size-16 rounded-full border-2 border-blue-500 object-cover object-center"
+          />
 
-//           <div className="flex flex-col">
-//             <h1
-//               className={cn(
-//                 "mt-4 text-2xl font-bold text-gray-800 uppercase",
-//                 caveat.className,
-//               )}
-//             >
-//               Cornelius
-//             </h1>
-//             <p className="-mt-1 text-sm text-gray-500">Web Developer</p>
-//           </div>
-//         </div>
+          <div className="flex flex-col">
+            <h1
+              className={cn(
+                "mt-4 text-2xl font-bold text-gray-800 uppercase",
+                caveat.className,
+              )}
+            >
+              Cornelius
+            </h1>
+            <p className="-mt-1 text-sm text-gray-500">Web Developer</p>
+          </div>
+        </div>
 
-//         <div className="p-4">
-//           {/* Navigation menu */}
-//           <nav className="mb-6">
-//             <ul className="space-y-2">
-//               {nav.map((item) => {
-//                 const isActive = pathname === item.link;
+        <div className="p-4">
+          {/* Navigation menu */}
+          <nav className="mb-6">
+            <ul className="space-y-2">
+              {nav.map((item) => {
+                const isActive = pathname === item.link;
 
-//                 return (
-//                   <li key={item.link}>
-//                     <Link
-//                       href={item.link}
-//                       onClick={handleCloseMenu}
-//                       className={cn(
-//                         "flex items-center gap-3 rounded-md px-3 py-2 transition-colors hover:bg-gray-100",
-//                         isActive && "bg-primary/20",
-//                       )}
-//                     >
-//                       <span className="text-primary">{item.icon}</span>
-//                       <span>{item.name}</span>
-//                     </Link>
-//                   </li>
-//                 );
-//               })}
-//             </ul>
-//           </nav>
+                return (
+                  <li key={item.link}>
+                    <Link
+                      href={item.link}
+                      onClick={handleCloseMenu}
+                      className={cn(
+                        "flex items-center gap-3 rounded-md px-3 py-2 transition-colors hover:bg-gray-100",
+                        isActive && "bg-primary/20",
+                      )}
+                    >
+                      <span className="text-primary">{item.icon}</span>
+                      <span>{item.name}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
 
-//           {/* Social links */}
-//           <div className="mb-6 border-b pb-8">
-//             <h2 className="text-textColor/70 mb-3 font-bold">
-//               Connect with Me
-//             </h2>
-//             <div className="inline-block space-y-3">
-//               {socials.map((item) => (
-//                 <Link
-//                   key={item.name}
-//                   href={item.link}
-//                   onClick={handleCloseMenu}
-//                   className="flex items-center gap-2 rounded-full bg-gray-100 px-3 py-1 text-sm transition-colors hover:bg-gray-200"
-//                 >
-//                   <span>{item.icon}</span>
-//                   <span className="text-gray-700">{item.name}</span>
-//                 </Link>
-//               ))}
-//             </div>
-//           </div>
+          {/* Social links */}
+          <div className="mb-6 border-b pb-8">
+            <h2 className="text-textColor/70 mb-3 font-bold">
+              Connect with Me
+            </h2>
+            <div className="inline-block space-y-3">
+              {socials.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.link}
+                  onClick={handleCloseMenu}
+                  className="flex items-center gap-2 rounded-full bg-gray-100 px-3 py-1 text-sm transition-colors hover:bg-gray-200"
+                >
+                  <span>{item.icon}</span>
+                  <span className="text-gray-700">{item.name}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
 
-//           <div className="flex items-center justify-between gap-4">
-//             {/* Resume button */}
-//             <button
-//               onClick={handleResumeClick}
-//               className="hover:bg-primary/50 bg-primary flex items-center justify-center gap-1 self-start rounded-full border px-3 py-2 text-sm font-medium whitespace-nowrap text-white transition-colors"
-//             >
-//               Read Resume <ChevronRight className="size-2" />
-//             </button>
+          <div className="flex items-center justify-between gap-4">
+            {/* Resume button */}
+            <button
+              onClick={handleResumeClick}
+              className="hover:bg-primary/50 bg-primary flex items-center justify-center gap-1 self-start rounded-full border px-3 py-2 text-sm font-medium whitespace-nowrap text-white transition-colors"
+            >
+              Read Resume <ChevronRight className="size-2" />
+            </button>
 
-//             <span>
-//               <Sun />
-//             </span>
-//           </div>
-//         </div>
-//       </header>
+            <span>
+              <Sun />
+            </span>
+          </div>
+        </div>
+      </header>
 
-//       <MenuToggleButton isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
-//     </>
-//   );
-// };
+      <MenuToggleButton isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+    </>
+  );
+};
 
-// export default Header;
+export default Header;
