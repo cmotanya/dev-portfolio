ab2389fb5081060269d788b4764504d38ef17492
Cornelius Motanya <corneliusmot@yahoo.com>
2025-04-27 11:53:50 +0300

    refactor: Clean up font imports and comment out Header and Footer components

--- app/data/font.ts
+++ app/data/font.ts
@@ -1,9 +1,4 @@
-import {
-  Bricolage_Grotesque as Grotesque,
-  Caveat_Brush as Caveat,
-  Poppins,
-  Nunito,
-} from "next/font/google";
+import { Bricolage_Grotesque as Grotesque, Poppins } from "next/font/google";
 
 export const grotesque = Grotesque({
   variable: "--font-geist-sans",
@@ -14,16 +9,4 @@ export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
-
-export const nunito = Nunito({
-  variable: "--font-geist-serif",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
-
-export const caveat = Caveat({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  weight: "400",
 });
