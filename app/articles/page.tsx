b1754ecc7cf3ab907405afcff3b0e1c3f3c2c765
Cornelius Motanya <corneliusmot@yahoo.com>
2025-05-20 09:18:20 +0300

    refactor: Remove ConsultationRequest component and update Articles page with new article structure

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,5 +1,6 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
-import Appointment from "../sample";
 
 const Article = () => {
   return (
@@ -14,7 +15,27 @@ const Article = () => {
         </h1>
       </div>
 
-      <Appointment />
+      <div className="flex flex-col gap-8">
+        <article className="flex flex-col gap-1">
+          <h2 className="text-2xl font-semibold">
+            See in the Dark: ColorVu 3.0 CCTV Cameras
+          </h2>
+          <p className="line-clamp-2 text-gray-700">
+            Discover how CCTV cameras with ColorVu 3.0 technology deliver vivid,
+            full-color video even in complete darkness. With advanced sensors
+            and smart imaging, ColorVu 3.0 ensures critical details are always
+            visible, enhancing security for homes and businesses day and night.
+          </p>
+          <Link
+            href="#"
+            className="bg-primary hover:bg-primary ring-secondary text-xs-sm flex max-w-fit items-center gap-2 rounded-full p-2 shadow-md ring transition duration-200 ease-in-out hover:-translate-y-1 focus:outline-none active:-translate-y-1"
+          >
+            Read more
+            <ArrowRight size={12} />
+          </Link>
+        </article>
+        {/* Add more articles as needed */}
+      </div>
     </section>
   );
 };
