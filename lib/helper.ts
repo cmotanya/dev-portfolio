8a3e4f72ea42def3c9a9ffc447d657d750b3ed05
Cornelius Motanya <corneliusmot@yahoo.com>
2025-05-25 18:27:56 +0300

    refactor: Comment out unused functions and types in helper.ts to improve code clarity

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,136 +1,136 @@
-import { SelectedItem, ServiceItem } from "@/app/data/service-quotation";
-import { Dispatch, SetStateAction } from "react";
+// import { SelectedItem, ServiceItem } from "@/app/data/service-quotation";
+// import { Dispatch, SetStateAction } from "react";
 
-// add item
-type AddItemParams = {
-  item: ServiceItem;
-  selectedItems: SelectedItem[];
-  setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
-};
-export const AddItem = ({
-  item,
-  selectedItems,
-  setSelectedItems,
-}: AddItemParams) => {
-  const defaultOption: Record<string, string> = {};
+// // add item
+// type AddItemParams = {
+//   item: ServiceItem;
+//   selectedItems: SelectedItem[];
+//   setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
+// };
+// export const AddItem = ({
+//   item,
+//   selectedItems,
+//   setSelectedItems,
+// }: AddItemParams) => {
+//   const defaultOption: Record<string, string> = {};
 
-  if (item.options) {
-    Object.entries(item.options).forEach(([key, values]) => {
-      if (values) {
-        defaultOption[key] = values[0];
-      }
-    });
-  }
+//   if (item.options) {
+//     Object.entries(item.options).forEach(([key, values]) => {
+//       if (values) {
+//         defaultOption[key] = values[0];
+//       }
+//     });
+//   }
 
-  const existingItemIndex = selectedItems.findIndex(
-    (selected) =>
-      selected.id === item.id &&
-      JSON.stringify(selected.selectedOptions) ===
-        JSON.stringify(defaultOption),
-  );
+//   const existingItemIndex = selectedItems.findIndex(
+//     (selected) =>
+//       selected.id === item.id &&
+//       JSON.stringify(selected.selectedOptions) ===
+//         JSON.stringify(defaultOption),
+//   );
 
-  if (existingItemIndex !== -1) {
-    setSelectedItems((prev) =>
-      prev.map((selected, index) =>
-        index === existingItemIndex
-          ? { ...selected, quantity: selected.quantity + 1 }
-          : selected,
-      ),
-    );
-  } else {
-    setSelectedItems((prev) => [
-      ...prev,
-      {
-        ...item,
-        quantity: 1,
-        selectedOptions: defaultOption,
-      },
-    ]);
-  }
-};
+//   if (existingItemIndex !== -1) {
+//     setSelectedItems((prev) =>
+//       prev.map((selected, index) =>
+//         index === existingItemIndex
+//           ? { ...selected, quantity: selected.quantity + 1 }
+//           : selected,
+//       ),
+//     );
+//   } else {
+//     setSelectedItems((prev) => [
+//       ...prev,
+//       {
+//         ...item,
+//         quantity: 1,
+//         selectedOptions: defaultOption,
+//       },
+//     ]);
+//   }
+// };
 
-// remove item
-type RemoveItemsParam = {
-  index: number;
-  setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
-};
-export const removeItem = ({ index, setSelectedItems }: RemoveItemsParam) => {
-  setSelectedItems((prev) => prev.filter((_, i) => i !== index));
-};
+// // remove item
+// type RemoveItemsParam = {
+//   index: number;
+//   setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
+// };
+// export const removeItem = ({ index, setSelectedItems }: RemoveItemsParam) => {
+//   setSelectedItems((prev) => prev.filter((_, i) => i !== index));
+// };
 
-type UpdateItemProp = {
-  index: number;
-  optionKey: string;
-  optionValue: string;
-  setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
-};
-export const updateItemOption = ({
-  index,
-  optionKey,
-  optionValue,
-  setSelectedItems,
-}: UpdateItemProp) => {
-  setSelectedItems((prev) =>
-    prev.map((item, i) =>
-      i === index
-        ? {
-            ...item,
-            selectedOptions: {
-              ...item.selectedOptions,
-              [optionKey]: optionValue,
-            },
-          }
-        : item,
-    ),
-  );
-};
+// type UpdateItemProp = {
+//   index: number;
+//   optionKey: string;
+//   optionValue: string;
+//   setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
+// };
+// export const updateItemOption = ({
+//   index,
+//   optionKey,
+//   optionValue,
+//   setSelectedItems,
+// }: UpdateItemProp) => {
+//   setSelectedItems((prev) =>
+//     prev.map((item, i) =>
+//       i === index
+//         ? {
+//             ...item,
+//             selectedOptions: {
+//               ...item.selectedOptions,
+//               [optionKey]: optionValue,
+//             },
+//           }
+//         : item,
+//     ),
+//   );
+// };
 
-// update quantity
-type UpdateQuantityProp = {
-  index: number;
-  quantity: number;
-  setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
-};
-export const updateQuantity = ({
-  index,
-  quantity,
-  setSelectedItems,
-}: UpdateQuantityProp) => {
-  if (quantity <= 0) {
-    setSelectedItems((prev) => prev.filter((_, i) => i !== index));
-  } else {
-    setSelectedItems((prev) =>
-      prev.map((item, i) => (i === index ? { ...item, quantity } : item)),
-    );
-  }
-};
+// // update quantity
+// type UpdateQuantityProp = {
+//   index: number;
+//   quantity: number;
+//   setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
+// };
+// export const updateQuantity = ({
+//   index,
+//   quantity,
+//   setSelectedItems,
+// }: UpdateQuantityProp) => {
+//   if (quantity <= 0) {
+//     setSelectedItems((prev) => prev.filter((_, i) => i !== index));
+//   } else {
+//     setSelectedItems((prev) =>
+//       prev.map((item, i) => (i === index ? { ...item, quantity } : item)),
+//     );
+//   }
+// };
 
-// update custom price
-type customerPriceProps = {
-  index: number;
-  customPrice: number;
-  setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
-};
-export const updateCustomPrice = ({
-  index,
-  customPrice,
-  setSelectedItems,
-}: customerPriceProps) => {
-  setSelectedItems((prev) =>
-    prev.map((item, i) => (i === index ? { ...item, customPrice } : item)),
-  );
-};
+// // update custom price
+// type customerPriceProps = {
+//   index: number;
+//   customPrice: number;
+//   setSelectedItems: Dispatch<SetStateAction<SelectedItem[]>>;
+// };
+// export const updateCustomPrice = ({
+//   index,
+//   customPrice,
+//   setSelectedItems,
+// }: customerPriceProps) => {
+//   setSelectedItems((prev) =>
+//     prev.map((item, i) => (i === index ? { ...item, customPrice } : item)),
+//   );
+// };
 
-// calculate item amount
-export const calculateItemAmount = (item: SelectedItem) => {
-  const price = item.customPrice || item.basePrice;
-  return price * item.quantity;
-};
+// // calculate item amount
+// export const calculateItemAmount = (item: SelectedItem) => {
+//   const price = item.customPrice || item.basePrice;
+//   return price * item.quantity;
+// };
 
-// calculate total amount
-export const totalAmount = (selectedItems: SelectedItem[]) => {
-  return selectedItems.reduce(
-    (total, item) => total + calculateItemAmount(item),
-    0,
-  );
-};
+// // calculate total amount
+// export const totalAmount = (selectedItems: SelectedItem[]) => {
+//   return selectedItems.reduce(
+//     (total, item) => total + calculateItemAmount(item),
+//     0,
+//   );
+// };
